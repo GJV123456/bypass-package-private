78402d3bc0be5ed86f45dbbf461ba89a0cde629a
GJC <599953528@qq.com>
2024-09-11 16:29:11 +0800

    绕过包限制

--- src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java
+++ src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java
@@ -2,6 +2,7 @@ package com.github.blindpirate.extensions;
 
 public class CaptureSystemOutputExtension {
     public static Object newInstance(){
+
         return new CaptureSystemOutputExtension();
     }
 }
